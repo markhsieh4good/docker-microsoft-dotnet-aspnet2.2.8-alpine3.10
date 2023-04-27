5a94bc5c904a77cde22fdba43aabaa356a89960f
mark.hsieh <mark.hsieh@paizaroyal.com.tw>
2023-04-27 02:00:46 +0000

    add .Net support ref.s

--- utility_dotnet_support.sh
+++ utility_dotnet_support.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "start .NET runtime install"
+## ref.:
+# 1.. https://learn.microsoft.com/zh-tw/dotnet/core/tools/dotnet-install-script
+# 2.. https://learn.microsoft.com/zh-tw/dotnet/core/install/linux-scripted-manual#scripted-install
+# 3.. https://dotnet.microsoft.com/zh-cn/download/dotnet/2.2
 
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash --runtime aspnetcore --version 2.2.x
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash --runtime dotnet --version 2.2.x
