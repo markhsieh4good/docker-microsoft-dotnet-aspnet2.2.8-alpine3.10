6c1a88fff6de2a9419116fd0b5b27179bd80fc03
mark.hsieh <mark.hsieh@paizaroyal.com.tw>
2023-04-27 02:44:19 +0000

    update steps

--- utility_dotnet_support.sh
+++ utility_dotnet_support.sh
@@ -4,14 +4,17 @@ echo "start .NET runtime install"
 # 1.. https://learn.microsoft.com/zh-tw/dotnet/core/tools/dotnet-install-script
 # 2.. https://learn.microsoft.com/zh-tw/dotnet/core/install/linux-scripted-manual#scripted-install
 # 3.. https://dotnet.microsoft.com/zh-cn/download/dotnet/2.2
+# 4.. https://dotnet.microsoft.com/zh-cn/download/dotnet
 
 _PWD=`pwd`
-_DIST="/usr/local/bin"
+_DIST_ROOT="/usr/local"
+export DOTNET_ROOT="${_DIST_ROOT}"/dotnet
+export PATH="$PATH:${DOTNET_ROOT}:${DOTNET_ROOT}/tools"
 
-mkdir -p "${_DIST}"
-cd "${_DIST}"
+mkdir -p "${DOTNET_ROOT}/tools"
+cd "${DOTNET_ROOT}"
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh 
-bash ./dotnet-install.sh --runtime aspnetcore --version 2.2.8xx --install-dir "${_DIST}"
-bash ./dotnet-install.sh --runtime dotnet --version 2.2.8xx --install-dir "${_DIST}"
+bash ./dotnet-install.sh --runtime aspnetcore --version 2.2.8
+bash ./dotnet-install.sh --runtime dotnet --version 2.2.8 
 
 cd "${_PWD}"
